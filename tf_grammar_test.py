b6dffc4ad2c606bc4dad7bca485f23facd30884a
Dxq <chk0125@126.com>
2017-11-30 18:59:45 +0800

    models

--- tf_grammar_test.py
+++ tf_grammar_test.py
@@ -9,7 +9,7 @@ import numpy as np
 
 a = tf.placeholder(tf.float32, shape=[None, 2])
 with tf.Session() as sess:
-    zz = np.array([[1, 2.0],[1, 2],[1, 2]]).reshape(-1, 2)
+    zz = np.array([[1, 2.0], [1, 2], [1, 2]]).reshape(-1, 2)
     print(zz.shape)
     print(sess.run(a, feed_dict={a: zz}))
 
@@ -44,3 +44,37 @@ print(train_x)
 # with sv.managed_session(config=config) as sess:
 #     if sv.should_stop():
 #         break
+tf.contrib.layers.fully_connected()
+
+# LSTM
+tf.unstack(x, timesteps, 1)
+outputs = tf.stack(outputs)
+from tensorflow.contrib import rnn
+
+# convolutional_network 重点
+
+lstm_fw_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
+outputs, _, _ = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x, dtype=tf.float32)
+outputs, states = tf.contrib.rnn.static_rnn(lstm_cell, x, dtype=tf.float32,
+                                                sequence_length=seqlen)
+
+
+tf.equal()
+tf.cast()
+x = tf.layers.dense(x, units=6 * 6 * 128)
+x = tf.nn.tanh(x)
+
+x = tf.layers.conv2d_transpose(x, 64, 4, strides=2)
+
+tf.concat([disc_real, disc_fake], axis=0)
+
+tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+    logits=disc_concat, labels=disc_target))
+
+gen_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Generator')
+
+train_gen = optimizer_gen.minimize(gen_loss)
+
+train_disc = optimizer_disc.minimize(disc_loss, var_list=disc_vars)
+
+outputs = tf.gather(tf.reshape(outputs, [-1, n_hidden]), index)
